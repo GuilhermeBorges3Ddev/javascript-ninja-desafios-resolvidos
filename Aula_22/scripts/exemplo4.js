abfc88da9a9b68251e415827a13bdc1a575b4ccd
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2021-05-03 01:27:44 -0300

    exemplo4.js codificado e desafio22.js pronto para ser resolvido

--- Aula_22/scripts/exemplo4.js
+++ Aula_22/scripts/exemplo4.js
@@ -1,6 +1,33 @@
 (function(){
+    
     'use strict';
+    
     /**
-     * 
-     */
+     * Array-like(arguments, Nodelists e etc) como Arrays
+     * Array.prototype.forEach.call()
+     * Array.prototype.forEach.apply()
+    */
+
+    function myFunction() {
+        Array.prototype.forEach.call(arguments, function(item) {
+            console.log(item);
+        })
+        var result = Array.prototype.reduce.call(arguments, function(ac, actual){
+            return (ac + actual)
+        })
+        console.log(result);
+    }
+
+    function myFunction2() {
+        Array.prototype.forEach.apply(arguments, [function(item) {
+            console.log(item);
+        }])
+    }
+
+    myFunction(1, 2, 3, 4, 5, 6, 7, 8);
+
+    console.log("-------------------");
+
+    myFunction2(1, 2, 3, 4, 5);
+
 }());
