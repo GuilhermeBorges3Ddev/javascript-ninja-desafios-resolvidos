721338723d25d5932665d4988b616c8df6f4bb65
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2021-06-13 18:45:42 -0300

    2/6 da Aula 26 finalizados

--- Aula_26/scripts/exemplo2.js
+++ Aula_26/scripts/exemplo2.js
@@ -1,9 +1,27 @@
-(function(win, doc){
+(function(doc){
 
   'use strict';
 
   /**
-   *
+   * A propriedade ".nodeType" retorna um inteiro: (9 - document), (1 - element), (3 - text), (8 - comments) e (22 - documentFragment)
+   * A propriedade ".nodeValue" nos traz o valor de tags do tipo text e element.
+   * A propriedade ".nodeName" nos traz o nome do nó; exemplo: #comment, #text ou o nome da tag HTML(div, header e etc).
+   * Para consultarmos as Features presentes nos browsers basta acesar: http://caniuse.com
   */
 
-})(window, document);
+  var $main = doc.querySelector('.main');
+  var $mainContent = doc.querySelector('.main-content');
+
+  console.log($main.firstChild.nodeType);
+
+  console.log("------------------------");
+
+  console.log($mainContent.firstChild.nextSibling.nodeValue);
+
+  console.log("------------------------");
+
+  console.log($mainContent.firstChild.nextSibling.nodeName);
+
+  console.log("------------------------");
+
+})(document);
---
+++ Aula_26/scripts/exemplo3.js
@@ -0,0 +1,9 @@
+(function(doc){
+
+  'use strict';
+
+  /**
+   *
+  */
+
+}(document));
