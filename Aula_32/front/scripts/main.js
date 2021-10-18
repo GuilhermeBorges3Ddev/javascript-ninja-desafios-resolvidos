a4fae70100f07646573ad7109c0055c0053d5b82
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2021-10-17 23:12:49 -0300

    main.js finalizado com o GET usando o ajax.abort() para pegarmos somente o header, e introdução ao POST via Ajax iniciado

--- Aula_32/front/scripts/main.js
+++ Aula_32/front/scripts/main.js
@@ -5,18 +5,35 @@
   /**
    * Vamos aqui exemplificar Rest API com Express.
    * Para isso usamos Node.js no server side.
+   * Observações:
+   * ajax.abort() => Útil para casos onde queremos buscar somente os headers.
+   * O POST é bastante similar ao GET: ajax.open('POST', <url>)...
+   * Porém é necessário explicitar o headers com o método abaixo:
+   * ajax.setRequestHeader(<key>,<value>)
+   * Após isso enviamos o par chave/valor no .send():
+   * ajax.send('key1=value1')
+   * Para mais de um par chave/valor usamos:
+   * ajax.send('key1=value1&key2=value2')
   */
 
   var ajax = new XMLHttpRequest();
   ajax.open('GET', 'http://localhost:3000/user/guilherme');
+  /*
+    Se executarmos a linha abaixo com o 3º parâmetro 'false', estamos explicitando que queremos uma requisição sync:
+    ajax.open('GET', 'http://localhost:3000/user/guilherme', false);
+  */
   ajax.send();
 
-  ajax.addEventListener('readystatechange', function(e) {
+  ajax.onreadystatechange = function(e) {
+    if(ajax.readyState == 2) {
+      console.log("Headers OK!");
+      ajax.abort();
+    }
     if(ajax.readyState === 4) {
       console.log("Backend sucess :: ", e)
       console.log(ajax.responseText, ajax.status);
     }
-  }, false);
+  };
 
 
 })(document, window);
