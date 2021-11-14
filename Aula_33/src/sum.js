2c866836f939c1a5298403576621e26f7376208a
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2021-11-14 18:43:33 -0300

    Basic structure to test sum module with Mocha created

---
+++ Aula_33/src/sum.js
@@ -0,0 +1,9 @@
+(function() {
+
+  'use strict';
+
+  /**
+   *  TDD - Test Driven Development - Antes do código ser feito são codados os testes.
+  */
+
+})()
---
+++ Aula_33/test/sum.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Lembrando que estamos usando Mocha...
+ * Cada it() dentro do describe() refere-se à um teste diferente.
+ * Dentro do it() o primeiro parâmetro será a descrição do teste e o segundo será o
+ * método de callback que será chamado quando o teste for executado.
+*/
+
+describe('# SUM', function() {
+  it('');
+});
